dd2cdb3fc4e59e61bbcd350c768fc7281a2476b2
DESKTOP-2VQRS6A\mapinus <ksb9242@gmail.com>
2022-02-15 15:16:04 +0900

    commit

--- src/main/java/hello/core/CoreApplication.java
+++ src/main/java/hello/core/CoreApplication.java
@@ -3,7 +3,7 @@ package hello.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication	// 이 안에 ComponentScan이 들어가 있음....
 public class CoreApplication {
 
 	public static void main(String[] args) {
--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,11 +1,16 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{
     
     // 2. 주입된 memberRepository 넣어짐
     private final MemberRepository memberRepository;
     
-    // 1. AppConfig 실행 시 MemberServiceImpl 생성자를 통해 memberRepository가 주입이 됌  
+    // 1. AppConfig 실행 시 MemberServiceImpl 생성자를 통해 memberRepository가 주입이 됌
+    @Autowired  // ac.getBean(MemberSRepository.class)
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
@@ -18,5 +23,9 @@ public class MemberServiceImpl implements MemberService{
     @Override
     public Member findMember(Long memberId) {
         return memberRepository.findById(memberId);
+    }
+    // 테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
     }
 }
